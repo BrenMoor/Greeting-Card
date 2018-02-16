96560d8c675a9726df7cff778b1cd46417a1a8ae
BrenMoor <36276065+BrenMoor@users.noreply.github.com>
2018-02-16 09:42:55 -0500

    Add project files.

---
+++ Greeting Card Good/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Greeting_Card_Good
+{
+    partial class lepusConstellation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lepusConstellation));
+            this.labelLepusTitle = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelParagraph = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelLepusTitle
+            // 
+            this.labelLepusTitle.AutoSize = true;
+            this.labelLepusTitle.Font = new System.Drawing.Font("Futura-Heavy", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLepusTitle.Location = new System.Drawing.Point(425, 339);
+            this.labelLepusTitle.Name = "labelLepusTitle";
+            this.labelLepusTitle.Size = new System.Drawing.Size(1034, 121);
+            this.labelLepusTitle.TabIndex = 0;
+            this.labelLepusTitle.Text = "LEPUS CONSTELLATION";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Myriad Hebrew", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(793, 488);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(291, 39);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Brendan Moorehead";
+            // 
+            // labelParagraph
+            // 
+            this.labelParagraph.AutoSize = true;
+            this.labelParagraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelParagraph.Location = new System.Drawing.Point(246, 708);
+            this.labelParagraph.Name = "labelParagraph";
+            this.labelParagraph.Size = new System.Drawing.Size(1335, 128);
+            this.labelParagraph.TabIndex = 2;
+            this.labelParagraph.Text = resources.GetString("labelParagraph.Text");
+            this.labelParagraph.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lepusConstellation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Greeting_Card_Good.Properties.Resources.Stars;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.labelParagraph);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelLepusTitle);
+            this.Name = "lepusConstellation";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLepusTitle;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelParagraph;
+    }
+}
+
---
+++ Greeting Card Good/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greeting_Card_Good
+{
+    public partial class lepusConstellation : Form
+    {
+        public lepusConstellation()
+        {
+            InitializeComponent();
+            //Start with paragraph invisible
+            labelParagraph.Visible = false;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Setting up graphics
+            Graphics g = this.CreateGraphics();
+            //Creating brush for stars
+            SolidBrush starBrush = new SolidBrush(Color.White);
+            //Creating brush for lines
+            Pen linePen = new Pen(Color.BlueViolet, 5);
+            //Make labels disappear
+            labelLepusTitle.Visible = false;
+            labelName.Visible = false;
+            //Title font
+            Font titleFont = new Font("Futura", 35, FontStyle.Bold);
+            SolidBrush titleBrush = new SolidBrush(Color.White);
+            //Body text font
+            Font bodyFont = new Font("Times New Roman", 12, FontStyle.Bold);
+            SolidBrush bodyBrush = new SolidBrush(Color.White);
+            //Clear Background
+            g.Clear(Color.Black);
+            //Line connecting Star 6 and Star 9
+            g.DrawLine(linePen, 377, 135, 433, 112);
+            //Line connecting Star 9 and Star 7
+            g.DrawLine(linePen, 433, 112, 488, 124);
+            //Line connecting Star 7 and Star 2
+            g.DrawLine(linePen, 488, 124, 575, 184);
+            //Line connecting Star 2 and Star 5
+            g.DrawLine(linePen, 575, 184, 697, 151);
+            //Line connecting Star 2 and Star 3
+            g.DrawLine(linePen, 575, 184, 613, 289);
+            //Line connecting Star 5 and Star 11
+            g.DrawLine(linePen, 700, 151, 669, 79);
+            //Line connecting Star 5 and Star 12
+            g.DrawLine(linePen, 701, 151, 715, 70);
+            //Line connecting Star 5 and Star 4
+            g.DrawLine(linePen, 701, 151, 754, 319);
+            //Line connecting Star 4 and Star 3
+            g.DrawLine(linePen, 754, 319, 612, 289);
+            //Line connecting Star 3 and Star 8
+            g.DrawLine(linePen, 612, 289, 507, 315);
+            //Line connecting Star 8 and Star 10
+            g.DrawLine(linePen, 507, 315, 466, 266);
+            //Line connecting Star 10 and Star 6
+            g.DrawLine(linePen, 466, 266, 379, 134);
+            //Line connecting Star 10 and Star 2
+            g.DrawLine(linePen, 466, 266, 572, 186);
+            //Star 6
+            g.FillEllipse(starBrush, 375, 130, 10, 10);
+            //Star 9
+            g.FillEllipse(starBrush, 430, 107, 10, 10);
+            //Star 7
+            g.FillEllipse(starBrush, 485, 120, 10, 10);
+            //Star 2
+            g.FillEllipse(starBrush, 570, 180, 10, 10);
+            //Star 5
+            g.FillEllipse(starBrush, 695, 145, 10, 10);
+            //Star 11
+            g.FillEllipse(starBrush, 665, 75, 10, 10);
+            //Star 12
+            g.FillEllipse(starBrush, 710, 68, 10, 10);
+            //Star 4
+            g.FillEllipse(starBrush, 750, 315, 10, 10);
+            //Star 3
+            g.FillEllipse(starBrush, 610, 285, 10, 10);
+            //Star 8 
+            g.FillEllipse(starBrush, 505, 310, 10, 10);
+            //Star 10
+            g.FillEllipse(starBrush, 460, 260, 10, 10);
+            //Star 1
+            g.FillEllipse(starBrush, 620, 350, 10, 10);
+            //Lepus constellation title
+            g.DrawString("LEPUS CONSTELLATION", titleFont, titleBrush, 310, 375);
+            //Make paragraph visible
+            labelParagraph.Visible = true;
+        }
+    }
+}
